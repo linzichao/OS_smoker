a6d671c7588e458b020624d79fc485ba2eccfa4e
biceneutron <biceneutron@gmail.com>
2017-11-26 14:04:32 +0800

    Add waiting time of Poisson distribution, progress bar, fix some  cmd prompt bugs.

--- src/application/SmokersAgents.java
+++ src/application/SmokersAgents.java
@@ -8,7 +8,7 @@
 
 package application;
 
-import java.util.Random;
+//import java.util.Random;
 import org.apache.commons.math3.distribution.PoissonDistribution; 
 
 /// GUI Library
@@ -23,21 +23,31 @@ import java.util.ResourceBundle;
 
 public class SmokersAgents{
 
-
 	public static final int TOBACCO = 0;
 	public static final int PAPER = 1;
 	public static final int MATCH = 2; 
-	public static Table table;
-	//public static PoissonDistribution poissonDistribution = new PoissonDistribution(3);
 
 	public static void main(String[] args){
 
-		table = new Table();
+		// For cmd line, use args.length != 3		
+		// For eclipse, use args.length != 1
+		if(args.length != 1) {
+			System.out.println("Usage: java SmokersAgents [Poisson distribution mean value]");
+			return;
+		}
+		
+		 // For cmd line, use args[2],		
+		 // For eclipse, use args[0], Run => Run Configurations => Arguments.
+		int meanWaitingTime = Integer.parseInt(args[0]);
+		System.out.println("Mean Waiting Time = " + meanWaitingTime);
+		Table table = new Table();
 
-		Smoker tobaccoSmoker =  new Smoker(table,"TOBACCO",TOBACCO);
-		Smoker paperSmoker = new Smoker(table,"PAPER",PAPER); 
-		Smoker matchSmoker = new Smoker(table,"MATCH",MATCH);
+		// Create smkrs.
+		Smoker tobaccoSmoker =  new Smoker(table,"TOBACCO",TOBACCO, meanWaitingTime);
+		Smoker paperSmoker = new Smoker(table,"PAPER",PAPER, meanWaitingTime); 
+		Smoker matchSmoker = new Smoker(table,"MATCH",MATCH, meanWaitingTime);
 
+		// Create agts.
 		Agent tobaccoAgent = new Agent(table, "TOBACCO");
 		Agent paperAgent = new Agent(table, "PAPER");
 		Agent matchAgent = new Agent(table, "MATCH");
@@ -51,52 +61,49 @@ public class SmokersAgents{
 		tobaccoSmoker.start();
 		paperSmoker.start();
 		matchSmoker.start();
-
-	}
+	}	
 }
 
 
 class Table {
-	private boolean[] tableIngred = new boolean[3]; // [TOBACCO, PAPER, MATCH]	
+	private boolean[] tableIngred = new boolean[3]; // [TOBACCO, PAPER, MATCH].	
 	private int numIngredInTable;
-
 
 	synchronized void getIngred(String ingred) {
 		if(numIngredInTable < 2) {
-			/* agent puts its ingredient on the table */
-			if(ingred == "TOBACCO" && tableIngred[0] == false) {
+			/* agent puts its ingredient on the table. */
+			if(ingred.equals("TOBACCO") && tableIngred[0] == false) {
 				tableIngred[0] = true;
 				System.out.print("TOBACCO ");
 			}
-			else if(ingred == "PAPER" && tableIngred[1] == false) {
+			else if(ingred.equals("PAPER") && tableIngred[1] == false) {
 				tableIngred[1] = true;
 				System.out.print("PAPER ");
 			}
-			else if(ingred == "MATCH" && tableIngred[2] == false) {
+			else if(ingred.equals("MATCH") && tableIngred[2] == false) {
 				tableIngred[2] = true;
 				System.out.print("MATCH ");
 			}
 			numIngredInTable ++;
 		}
 		try {
-			System.out.print(" " + ingred + "Waiting ");
+			
+			/* agt blocks here. */
 			wait();
-			System.out.print(" " + ingred + "Notified ");
+			/*******************/
+			
 		} catch(Exception e) {}
 	}
 
 	synchronized boolean giveIngred(String smkrName) {
 		if(numIngredInTable == 2) {
-			if(smkrName == "TOBACCO" && tableIngred[0] == false) {
-				/* matches */
+			if(smkrName.equals("TOBACCO") && tableIngred[0] == false) { // if smkrTOBACCO comes in , and no tobacco on the table.
 				return true;
 			}
-			else if(smkrName == "PAPER" && tableIngred[1] == false) {
-				/* matches */
+			else if(smkrName.equals("PAPER") && tableIngred[1] == false) { // if smkrPAPER comes in , and no paper on the table.
 				return true;
 			}
-			else if(smkrName == "MATCH" && tableIngred[2] == false) {
-				/* matches */
+			else if(smkrName.equals("MATCH") && tableIngred[2] == false) { // if smkrMATCH comes in , and no match on the table.
 				return true;
 			}
 		}
@@ -104,16 +111,86 @@ class Table {
 	}
 
 	synchronized void consumeIngred() {
+		// set table to empty.
 		tableIngred[0] = false;
 		tableIngred[1] = false;
 		tableIngred[2] = false;
 		numIngredInTable = 0;
-		System.out.println("==============================");
+		System.out.println("\n==============================");
 		System.out.print("Table now has: ");
 	}
 
 	synchronized void notifyAllPeople() {
 		notifyAll();
+	}
+}
+
+class Smoker extends Thread{
+	Table table;
+	int myIngred;
+	String name;
+	int meanWaitingTime;
+	PoissonDistribution pd;
+
+	/* constructor */
+	public Smoker(Table table, String name ,int myIngred, int meanWaitingTime){
+		this.table = table;
+		this.name = name;
+		this.myIngred = myIngred;
+		this.meanWaitingTime = meanWaitingTime;
+		pd = new PoissonDistribution(meanWaitingTime);
+	}
+
+	public void run(){
+		while(true) {
+			getIngred();
+		}
+	}
+
+	synchronized void getIngred() {
+
+		if(table.giveIngred(name) == true) {
+			// if it's the right smoker, go smoking, and then notify all agents.
+			smoke();
+			table.notifyAllPeople(); // 2 or 3 agts are waiting in table now, you need to wake'em up.
+		}
+	}
+
+	synchronized void smoke() {
+		try {
+			int waitingTime;
+			// Let waitingTime > 0.
+			while(true) {
+				waitingTime = pd.sample();
+				if(waitingTime != 0) {
+					break;
+				}
+			}
+			
+			System.out.print("\n" + name + "_owner is smoking, wait for " + waitingTime + " sec");
+			sleepLoadingBar(waitingTime);
+			table.consumeIngred();
+		} catch(Exception e) {}
+	}
+	
+	synchronized void sleepLoadingBar(int time) {
+		try {
+			int waitingTimePerRound = time * 1000 / 100;
+			String strLeft = "|";
+			String strRight = "                                                                                                    |";
+			
+			System.out.println();
+			for(int i = 0; i < 100; i++) {
+				System.out.print(strLeft + strRight.substring(i) + "    " + i + "%\r");
+				//System.out.print("DB_0");
+				strLeft += "*";
+				
+				/* smkr sleeps here. */
+				Thread.sleep(waitingTimePerRound);
+				/********************/
+			}
+			System.out.print(strLeft + "|" + "    " + "100%");
+		} catch(Exception e) {}			
 	}
 }
 
@@ -131,13 +208,11 @@ class Agent extends Thread implements Initializable{
     	
     }
 	
-	
 	/* constructor */
 	public Agent(Table table, String name) {
 		this.table = table;
 		this.myIngred = name;
 	}
-
 
 	public void run() {
 		//tobacco.setImage(new Image("file:src/image/match.png"));
@@ -148,12 +223,10 @@ class Agent extends Thread implements Initializable{
 	}
 
 	synchronized void putIngred() {
-		//synchronized(table) {
-			/* the agent try to put ingred onto table, then it waits.
-			 * If: 1. The table is available, the agent put and then wait.
-			 *	   2. The table is full, the agent just wait. */
-			table.getIngred(myIngred);
-		//}
+		// The agt tries to put ingred onto table, then it waits.
+		// If: 1. The table is available, the agt puts and then waits.
+		//	   2. The table is full, the agt just waits.
+		table.getIngred(myIngred);
 	}
 	
 	/* GUI Image motion */
@@ -169,63 +242,4 @@ class Agent extends Thread implements Initializable{
     }
 	
 }
-
-
-class Smoker extends Thread{
-	Table table;
-	int my_Ingred;
-	String name;
-
-	/* constructor */
-	public Smoker(Table table, String name ,int my_Ingred){
-		this.table = table;
-		this.name = name;
-		this.my_Ingred = my_Ingred;			
-	}
-
-	public void run(){
-		while(true) {
-			//System.out.println(name + " is in run()");
-			getIngred();
-			//System.out.println("DB_0");
-		}
-	}
-
-	synchronized void getIngred() {
-		//synchronized(table) {
-
-			if(table.giveIngred(name) == true) {
-				//System.out.println("DB_1");
-				// if it's the right smoker, go smoking, and then notify all smokers and agents
-				smoke();
-				table.notifyAllPeople();
-			}
-		//}
-	}
-
-	synchronized void smoke() {
-		try {
-			System.out.println("\n" + name + "_owner is smoking");
-			Thread.sleep(2000);
-			table.consumeIngred();
-				//System.out.println("DB_3");
-		} catch(Exception e) {}
-	}
-}
-/*
-
-   if(code == need){
-   String smoker_own ="";
-   if(code == 0){
-   smoker_own = "TABACCO";
-   }
-   if(code == 1){
-   smoker_own = "PAPER";
-   }
-   if(code == 2){
-   smoker_own = "MATCH";
-   }
-   System.out.println("==================");
-   System.out.println("Agent delivers: "+ in_table +" in the table");
-   System.out.println("Smoker : " +name+" has "+smoker_own+" and is ready to smoke");
- */
+   
